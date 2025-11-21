e9b325944be76767db60644e9cf2c07377b80bd2
hyun-98 <126747395+hyun-98@users.noreply.github.com>
2025-11-21 17:12:25 +0900

    add HomeController

--- backend/church/src/main/java/com/example/church/controller/HomeController.java
+++ backend/church/src/main/java/com/example/church/controller/HomeController.java
@@ -1,15 +1,14 @@
 package com.example.church.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    // 모든 경로를 React index.html로 포워딩
-    @RequestMapping(value = {"/", "/attendance", "/register", "/report-create", "/stats"})
-    public String forwardReactRoutes() {
+    // 점(.)이 없는 모든 경로는 index.html로 포워딩
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
         return "forward:/index.html";
     }
 }

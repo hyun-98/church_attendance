f08126155ef385bbfe359d85125937035cb14163
hyun-98 <126747395+hyun-98@users.noreply.github.com>
2025-11-25 17:27:10 +0900

    local 설정 완료

--- backend/church/src/main/java/com/example/church/config/WebConfig.java
+++ backend/church/src/main/java/com/example/church/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.church.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // C:/church/uploads/** 경로를 /uploads/** URL로 매핑
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///C:/church/uploads/");
+                .addResourceLocations("file:" + uploadDir + "/");
     }
 }

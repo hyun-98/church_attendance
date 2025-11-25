a82d450571914231f73b8a7213966898c671957b
hyun-98 <126747395+hyun-98@users.noreply.github.com>
2025-11-25 22:50:11 +0900

    Security config fix

--- backend/church/src/main/java/com/example/church/config/SecurityConfig.java
+++ backend/church/src/main/java/com/example/church/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.church.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity   // 🔥 SecurityConfig 강제 활성화
 public class SecurityConfig {
 
     @Bean
